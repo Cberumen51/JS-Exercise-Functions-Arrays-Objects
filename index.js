fa024443119102a94f1fe2aef521a8f2f58a2efb
Cberumen51 <c.berumen51@gmail.com>
2019-10-30 19:32:52 -0700

    tried

--- index.js
+++ index.js
@@ -37,8 +37,13 @@ function exampleFunction(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(id, name, email) {
+  var person = {
+    id: id,
+    name: name,
+    email: email,
+  };
+return person;
 }
 
 /**
@@ -54,9 +59,12 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
-}
+function getName(name) {
+  var person = {
+    name: name
+  };
+  console.log('Hello, my name is' person.name)
+
 
 /**
  * ### Challenge `makeSmartPerson`
